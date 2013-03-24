0cb1c9427e85f639624c05fa58976eb3c4dc092e
root <root@ubuntu.(none)>
2013-03-24 18:10:16 -0400

    edited

--- custom/topo-2sw-2host.py
+++ custom/topo-2sw-2host.py
@@ -23,12 +23,13 @@ class MyTopo( Topo ):
         leftHost = self.addHost( 'h1' )
         rightHost = self.addHost( 'h2' )
         leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
+        downSwitch = self.addSwitch( 's4' )
+	downHost = self.addHost( 'h5' )
 
         # Add links
         self.addLink( leftHost, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch )
-        self.addLink( rightSwitch, rightHost )
-
+        self.addLink( leftSwitch, downSwitch )
+        self.addLink( downSwitch, downHost )
+	self.addLink( leftSwitch, rightHost )
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
